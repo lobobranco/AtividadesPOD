0a271c01c84a2c7f9953ef55167a7e804899f876
Lucas Galdino <31944849+lobobranco@users.noreply.github.com>
2019-08-19 10:49:11 -0300

    Iniciando de forma correta

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import { bubbleSort, bubbleSortRevenge, optimunBubbleSort } from './sorting/bubbleSort'
+
+let numbers = [13, 8, 7, 6, 2, 1]
+let numbers2 = [1, 2, 6, 7, 8, 13]
+
+optimunBubbleSort(numbers)
+
+console.log(numbers)
